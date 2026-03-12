863ee564c18ad58465333ce9cab2a94ec3a69d1f
madnessqw <madnessqw@users.noreply.github.com>
2026-03-12 17:39:51 +0300

    fix proj-fgwmjycp review

--- calc.py
+++ calc.py
@@ -1,10 +1,2 @@
-   def add(a, b):
-       return a + b
-   EOF
-
-   cat > test_calc.py <<'EOF'
-   from calc import add
-
-   def test_add():
-       assert add(2, 3) == 5
-   EOF
+def add(a, b):
+    return a + b
